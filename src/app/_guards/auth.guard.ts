6db9408895ebd91994d42d23f5e1c2c60cba3dba
IggyBlob <socialmedia@paulhaunschmied.com>
2018-01-05 15:28:29 +0100

    Extend AuthGuard to verify JWT

--- src/app/_guards/auth.guard.ts
+++ src/app/_guards/auth.guard.ts
@@ -7,11 +7,28 @@ export class AuthGuard implements CanActivate {
     constructor(private router: Router) { }
 
     canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot) {
-        if (localStorage.getItem('token')) {
-            // logged in since token is available
-            // TODO: check JWT expiration field
-            return true;
+        const token = localStorage.getItem('token');
+        if (token) {
+            const payload = token.split('.');
+            if (payload.length === 3) {
+                try {
+                    const payloadObj = JSON.parse(atob(payload[1]));
+                    if (payloadObj && payloadObj.exp) {
+                        if (Math.floor((new Date()).getTime() / 1000) < payloadObj.exp) {
+                            // logged in and valid JWT in localStore
+                            console.log('valid JWT');
+                            console.log(payloadObj);
+                            console.log(Math.floor((new Date()).getTime() / 1000));
+                            console.log(payloadObj.exp);
+                            return true;
+                        }
+                    }
+                } catch (e) {}
+            }
+            // token invalid, so remove it
+            localStorage.removeItem('token');
         }
+
         // not logged in
         this.router.navigateByUrl('/login');
         return false;
