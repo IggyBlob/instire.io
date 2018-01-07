9f78386b09f995191a827a2522c31871b27ff370
IggyBlob <socialmedia@paulhaunschmied.com>
2018-01-07 15:24:27 +0100

    Add counter animation

--- src/app/scoreboard/scoreboard.component.ts
+++ src/app/scoreboard/scoreboard.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import {UserService} from '../_services/user.service';
+import { UserService } from '../_services/user.service';
 
 @Component({
   selector: 'app-scoreboard',
@@ -9,18 +9,26 @@ import {UserService} from '../_services/user.service';
 export class ScoreboardComponent implements OnInit {
 
     loading: boolean;
+    scoreCount: number;
 
     constructor(public userService: UserService) {
         this.loading = true;
+        this.scoreCount = 0.0;
     }
 
     ngOnInit() {
-
         this.userService.fetchUser()
             .then(() => this.userService.fetchScore())
-            .then(() => this.loading = false);
+            .then(() => {
+                this.loading = false;
+                const interval = setInterval(() => {
+                    this.scoreCount += 0.11;
+                    if (this.scoreCount > this.userService.user.score) {
+                        clearInterval(interval);
+                        this.scoreCount = this.userService.user.score;
+                    }
+                }, 10);
+            });
 
-        //this.userService.fetchScore();
     }
-
 }
